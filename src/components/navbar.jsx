d1bf1c268f2187828584443a99ec0a7a2e901c94
Jacky Andrazat <82565381+Jackyandrazat@users.noreply.github.com>
2024-04-15 11:49:04 +0700

    setup package

---
+++ src/components/navbar.jsx
@@ -0,0 +1,34 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+export const navbarComponent = () => {
+  return (
+    <div>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>Link</a>
+            </li>
+            <li>
+              <details>
+                <summary>Parent</summary>
+                <ul className="p-2 bg-base-100 rounded-t-none">
+                  <li>
+                    <a>Link 1</a>
+                  </li>
+                  <li>
+                    <a>Link 2</a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
